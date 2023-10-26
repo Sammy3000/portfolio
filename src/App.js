5a3d9968e64e0b3614978b9f56959c7afabc5f6c
Sammy3000 <towettsammy30@gmail.com>
2023-10-26 13:07:34 +0300

    Import Contacts to parent component app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './index.css'
 import Home from './routes/Home';
 import Projects from './routes/Projects';
 import Contact from './routes/Contact';
+import About from './routes/About';
 
 
 function App() {
