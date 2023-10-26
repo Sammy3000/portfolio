32478c488233bc83c0c8239e96063c645f4e6ebd
Sammy3000 <towettsammy30@gmail.com>
2023-10-26 13:45:05 +0300

    Create a ul with links to each component of the menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,12 @@ const Navbar = () => {
         <Link to='/'>
             <h1>Portfolio</h1>
         </Link>
+        <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='project'>Projects</Link></li>
+            <li><Link to='about'>About</Link></li>
+            <li><Link to='contact'>Contact</Link></li>
+        </ul>
 
     </div>
   )
