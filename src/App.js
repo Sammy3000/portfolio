7c83f140feccc33afbd351065f6dd23332bef39a
Sammy3000 <towettsammy30@gmail.com>
2023-10-26 13:11:26 +0300

    Import Contacts to parent component app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Home from './routes/Home';
 import Projects from './routes/Projects';
 import Contact from './routes/Contact';
 import About from './routes/About';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
-  <div>
-    <h1>Hello react</h1>
-  </div>
+ <>
+ <Routes>
+  <Route path='/' element={<Home/>}/>
+ </Routes>
+ </>
   );
 }
 
