916859a09dcabd8e9c111e204dfff596d08ab85a
Sammy3000 <towettsammy30@gmail.com>
2023-10-29 12:18:33 +0300

    Modify styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
         <Link to='/'>
             <h1>Portfolio</h1>
         </Link>
-        <ul className='nav-menu'>
+        <ul className={click?'nav-menu active':'nav-menu'}>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='project'>Projects</Link></li>
             <li><Link to='about'>About</Link></li>
