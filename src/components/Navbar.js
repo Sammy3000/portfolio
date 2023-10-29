9368d2ab02ed8ecca49ef7e87571daca65a40fdb
Sammy3000 <towettsammy30@gmail.com>
2023-10-29 12:06:01 +0300

    Render icons conditionally

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,10 +19,9 @@ const Navbar = () => {
             <li><Link to='about'>About</Link></li>
             <li><Link to='contact'>Contact</Link></li>
         </ul>
-        <div className='hambuger'>
-          <FaTimes size={20} style={{color:'#fff'}}/>
-          <FaBars size={20} style={{color:'#fff'}}/>
-/
+        <div onClick={handleClick} className='hambuger'>
+          {click?(<FaTimes size={20} style={{color:'#fff'}}/>):(<FaBars size={20} style={{color:'#fff'}}/>)}
+          
         </div>
 
     </div>
