bc5712b6c195bbf206d6669a675080dab91cf984
Sammy3000 <towettsammy30@gmail.com>
2023-11-01 09:44:57 +0300

    Add content to the component

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HeroImgStyle.css';
 import ImageOne from '../assets/code1.jpg';
+import {Link} from 'react-router-dom';
 
 const HeroImg = () => {
   return (
@@ -8,7 +9,15 @@ const HeroImg = () => {
       <div className='mask'>
         <img className='intro-image' src={ImageOne} alt='IntroImage'/>
       </div>
-
+      <div className='content'>
+        <p>Hi I'm a Developer</p>
+        <h1>React Developer</h1>
+        <div>
+          <Link to='project'></Link>
+          <Link to='contact'></Link>
+        </div>
+        
+      </div>
     </div>
   )
 }
