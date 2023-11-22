6c41aab7a729be1c6be04ae33550860bbea2e7ad
Sammy3000 <towettsamm30@gmail.com>
2023-11-22 12:19:46 +0300

    Modify footer and home components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,22 @@
 import './FooterStyles.css'
+import React from 'react' 
+import { FaHome } from 'react-icons/fa'
 
 const Footer=()=>{
-    
-    return{}
+
+    return(
+        <div className='footer'>
+            <div className='footer-container'>
+                <div className='left'>
+                    <div className='location'>
+                        <FaHome/>
+                    </div>
+                </div>
+                <div className='right'></div>
+            </div>
+
+        </div>
+    )
 
 }
 export default Footer
