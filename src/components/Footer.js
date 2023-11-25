fd9544c12354c34bd2353a80bf4c86cf1db3988b
Sammy3000 <towettsamm30@gmail.com>
2023-11-25 10:35:21 +0300

    Add mail address

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import './FooterStyles.css'
 import React from 'react' 
-import { FaHome } from 'react-icons/fa'
+import { FaHome, FaMailBulk, FaPhone } from 'react-icons/fa'
 
 const Footer=()=>{
 
@@ -10,6 +10,13 @@ const Footer=()=>{
                 <div className='left'>
                     <div className='location'>
                         <FaHome size={20} style={{color:'#fff',marginRight:'2rem'}}/>
+                    </div>
+                    <div className='phone'>
+                        <h4><FaPhone size={20} style={{color:'#fff',marginRight:'2rem'}}/>+254 743793661</h4>
+                    </div>
+                    <div className='email'>
+                      <h4><FaMailBulk size={20} style={{color:'#fff',marginRight:'2rem'}}/>towettsammy30@gmail.com
+                      </h4>  
                     </div>
                 </div>
                 <div className='right'></div>
