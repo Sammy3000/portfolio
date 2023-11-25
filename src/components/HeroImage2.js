8e77b8dfdc489981a015535b6d2ca4300d9305c5
Sammy3000 <towettsamm30@gmail.com>
2023-11-25 12:54:17 +0300

    Pass a component to projects componenent

--- src/components/HeroImage2.js
+++ src/components/HeroImage2.js
@@ -4,7 +4,7 @@ import React from 'react'
 const HeroImage2=()=>{
     return(
         <div>
-            
+            Hello
         </div>
     )
 }
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import HeroImage2 from '../components/HeroImage2'
 
 const Projects = () => {
   return (
     <div>
       <Navbar/>
+      <HeroImage2/>
       <Footer/>
     </div>
   )
