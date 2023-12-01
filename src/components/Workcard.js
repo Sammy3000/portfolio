aec47ce0a22842fabe0d5c18bdc51fcc01f958f4
Sammy3000 <towettsammy30@gmail.com>
2023-12-01 12:22:16 +0300

    Modify component

--- src/components/Workcard.js
+++ src/components/Workcard.js
@@ -3,7 +3,15 @@ import React from 'react'
 
 const Workcard = ()=>{
     return(
-        <div></div>
+        <div className='work-container'>
+            <h1 className='projects-heading'>Projects.</h1>
+            <div className='project-container'>
+                <div className='project-card'>
+                    <img src='' alt='Project Image'/>
+
+                </div>
+            </div>
+        </div>
     )
 }
 export default Workcard
