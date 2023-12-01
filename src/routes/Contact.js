650efaf0286f6de9ddc8718b5fee2e00b3bb2d89
Sammy3000 <towettsammy30@gmail.com>
2023-12-01 08:34:37 +0300

    Import hero image component into contact page

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import HeroImage2 from '../components/HeroImage2'
 
 const Contact = () => {
   return (
     <div>
       <Navbar/>
+      <HeroImage2/>
       <Footer/>
     </div>
   )
