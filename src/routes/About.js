de886fbd5fce097cb74a296a580ae87d378a4a47
Sammy3000 <towettsammy30@gmail.com>
2023-12-01 08:40:09 +0300

    Modify about page by passing props

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import HeroImage2 from '../components/HeroImage2'
 
 const About = () => {
   return (
     <div>
       <Navbar/>
+      <HeroImage2 heading={"ABOUT ME."} paragraph={"I am a Frontend Web Developer with passion and experience building the best User Interface with smooth interraction for users using React Js."}/>
       <Footer/>
     </div>
   )
