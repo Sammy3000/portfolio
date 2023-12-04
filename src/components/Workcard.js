06feaec77f160c68ac3c8bb19faa25c1f90284c9
Sammy3000 <towettsammy30@gmail.com>
2023-12-04 21:22:28 +0300

    Modify project details

--- src/components/Workcard.js
+++ src/components/Workcard.js
@@ -12,7 +12,7 @@ const Workcard = ()=>{
                     <img src={worldwide} alt='Project pic'/>
                     <h2 className='project-title'>Project Title</h2>
                     <div className='project-details'>
-                        I love doing react projects.
+                       <p>I love doing react projects.</p> 
                     </div>
                     <div className='project-btns'>
                         <NavLink className='btn' to='url.com'>View</NavLink>
