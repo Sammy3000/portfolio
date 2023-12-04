5d1c8075686bf899bbe0b74e3c23d90e99ba01eb
Sammy3000 <towettsammy30@gmail.com>
2023-12-04 21:36:32 +0300

    Copy workcard js component data to work component and modify name

---
+++ src/components/Work.js
@@ -0,0 +1,26 @@
+import './Workcard.css'
+import React from 'react'
+import worldwide from '../assets/mobile.png'
+import { NavLink } from 'react-router-dom'
+
+const Work = ()=>{
+    return(
+        <div className='work-container'>
+            <h1 className='projects-heading'>Projects.</h1>
+            <div className='project-container'>
+                <div className='project-card'>
+                    <img src={worldwide} alt='Project pic'/>
+                    <h2 className='project-title'>Project Title</h2>
+                    <div className='project-details'>
+                       <p>I love doing react projects.</p> 
+                    </div>
+                    <div className='project-btns'>
+                        <NavLink className='btn' to='url.com'>View</NavLink>
+                        <NavLink className='btn' to='url.com'>Source</NavLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Work
