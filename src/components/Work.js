9730e9bee6963ddea956823f84b3f66e31dac378
Sammy3000 <towettsammy30@gmail.com>
2023-12-04 21:39:19 +0300

    Remove  project card class from work component

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,17 +8,7 @@ const Work = ()=>{
         <div className='work-container'>
             <h1 className='projects-heading'>Projects.</h1>
             <div className='project-container'>
-                <div className='project-card'>
-                    <img src={worldwide} alt='Project pic'/>
-                    <h2 className='project-title'>Project Title</h2>
-                    <div className='project-details'>
-                       <p>I love doing react projects.</p> 
-                    </div>
-                    <div className='project-btns'>
-                        <NavLink className='btn' to='url.com'>View</NavLink>
-                        <NavLink className='btn' to='url.com'>Source</NavLink>
-                    </div>
-                </div>
+              
             </div>
         </div>
     )
