193048fd4d11cfbcb6cd12398bab536b2e316a8d
Sammy3000 <towettsammy30@gmail.com>
2023-12-06 06:42:23 +0300

    Finish up the first project data

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -3,6 +3,11 @@ import project2 from '../assets/space.png'
 import project3 from '../assets/peace.png'
 const ProjectCardData=[
     {
-        imgsrc:project1
+        imgsrc:project1,
+        title:'Wordwide',
+        text:'World wide app showing list of countries fetched from an external API. User can can click on a country to view all information they need. User can also search for a country directly using a search box.',
+        view:'https://towettsammy-worldwide.onrender.com/',
+        source:'https://github.com/Sammy3000/worldwide.git'
+
     },
 ]
