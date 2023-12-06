5ac464845c3dbb95178ce42212d171985f68c88a
Sammy3000 <towettsammy30@gmail.com>
2023-12-06 18:49:41 +0300

    Fill the left section with paragraph header and button

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './AboutContentStyles.css'
 import React from 'react'
 
@@ -5,12 +6,16 @@ const AboutContent=()=>{
     return(
         <div className='about'>
             <div className='left'>
-
+                <h1>Who am I?</h1>
+                <p>I'm a React front end developer. I create responsive secure websites for my clients.</p>
+                <Link to='/contact'>
+                    <button className='btn'>Contact</button>
+                </Link>
 
             </div>
             <div className='right'>
 
-                
+
             </div>
         </div>
     )
