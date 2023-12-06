8dbb8f2d4188b7000e45bebd8ccffff1f15c2341
Sammy3000 <towettsammy30@gmail.com>
2023-12-06 07:18:05 +0300

    Map component and pass data accordingly

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,7 +8,19 @@ const Work = ()=>{
         <div className='work-container'>
             <h1 className='projects-heading'>Projects.</h1>
             <div className='project-container'>
-              
+              {
+                WorkCardData.map((val,index)=>{
+                    return(
+                        <Workcard
+                        key={index}
+                        imgsrc={val.imgsrc}
+                        title={val.title}
+                        text={val.text}
+                        view={val.view}
+                        />
+                    )
+                })
+              }
             </div>
         </div>
     )
