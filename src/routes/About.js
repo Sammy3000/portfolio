73b981dd49f6d3046dc32c9766612758b2c84d42
Sammy3000 <towettsammy30@gmail.com>
2023-12-06 18:41:20 +0300

    Render about content inside about page

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroImage2 from '../components/HeroImage2'
+import AboutContent from '../components/AboutContent'
 
 const About = () => {
   return (
     <div>
       <Navbar/>
       <HeroImage2 heading={"ABOUT ME."} paragraph={"I am a Frontend Web Developer with passion and experience building the best User Interface with smooth interraction for users using React Js."}/>
+      <AboutContent/>
       <Footer/>
     </div>
   )
