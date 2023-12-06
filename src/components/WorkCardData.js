d1aff51790bbb871970863e5d8db8967769b7ddb
Sammy3000 <towettsammy30@gmail.com>
2023-12-06 06:28:40 +0300

    Import 3 more projects

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,4 +1,6 @@
 import project1 from '../assets/mobile.png'
+import project2 from '../assets/space.png'
+import project3 from '../assets/peace.png'
 const ProjectCardData=[
     {
         imgsrc:project1
