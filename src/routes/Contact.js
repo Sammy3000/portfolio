d91dcb0bc14c9118cfb070b29c2ed85e6a8c79c0
Sammy3000 <towettsammy30@gmail.com>
2023-12-07 08:02:07 +0300

    Render form component inside contact

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroImage2 from '../components/HeroImage2'
+import { Form } from 'react-router-dom'
 
 const Contact = () => {
   return (
     <div>
       <Navbar/>
       <HeroImage2 heading={"CONTACT"} paragraph={"Let's have a chat"}/>
+      <Form/>
       <Footer/>
     </div>
   )
