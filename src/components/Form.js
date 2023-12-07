2083c4054a3a9c2c88cf5e2caf9415a7f5b753a9
Sammy3000 <towettsammy30@gmail.com>
2023-12-07 08:31:22 +0300

    Create a form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,19 @@ import React from 'react'
 
 const Form=()=>{
     return(
-        <div></div>
+        <div className='form'>
+            <Form>
+               <label>Your Name</label>
+               <input type='text'></input>
+               <label>Email</label>
+               <input type='email'></input>
+               <label>Subject</label>
+               <input type='text'></input>
+               <label>Message</label>
+               <textarea rows='6' placeholder='Type your message here...'></textarea>
+               <button type='submit' className='btn'>Submit</button>
+            </Form>
+        </div>
     )
 }
 
