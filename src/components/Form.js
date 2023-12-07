06ce267a6b70ff656215f2c3226ff466c532ff24
Sammy3000 <towettsammy30@gmail.com>
2023-12-07 08:37:42 +0300

    Modify form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,17 +4,7 @@ import React from 'react'
 const Form=()=>{
     return(
         <div className='form'>
-            <Form>
-               <label>Your Name</label>
-               <input type='text'></input>
-               <label>Email</label>
-               <input type='email'></input>
-               <label>Subject</label>
-               <input type='text'></input>
-               <label>Message</label>
-               <textarea rows='6' placeholder='Type your message here...'></textarea>
-               <button type='submit' className='btn'>Submit</button>
-            </Form>
+        
         </div>
     )
 }
