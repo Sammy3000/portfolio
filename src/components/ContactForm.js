c91341a572c6608977c644b290263f8986494c89
Sammy3000 <towettsammy30@gmail.com>
2023-12-13 11:00:35 +0300

    Modify input name

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -28,13 +28,13 @@ const ContactForm=()=>{
         <div className='form'>
            <form ref={form} onSubmit={sendEmail}>
                 <label>Name</label>
-                <input type="text" name="from_name" />
+                <input className='input' type="text" name="from_name" />
                 <label>Email</label>
-                <input type="email" name="user_email" />
+                <input className='input' type="email" name="user_email" />
                 <label>Message</label>
                 <textarea name="message" />
                 <input type="submit" value="Send" />
-    </form>
+           </form>
         </div>
     )
 }
