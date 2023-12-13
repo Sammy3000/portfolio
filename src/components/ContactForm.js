29ab1c794e69824f5c52df0870cb547a1d67e5f1
Sammy3000 <towettsammy30@gmail.com>
2023-12-13 12:26:55 +0300

    Add and modify text area placeholder

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -32,7 +32,7 @@ const ContactForm=()=>{
                 <label>Email</label>
                 <input className='input' type="email" name="user_email" required/>
                 <label>Message</label>
-                <textarea name="message" required/>
+                <textarea placeholder='Write me a message...' name="message" required/>
                 <input className='button' type="submit" value="Send" />
            </form>
         </div>
