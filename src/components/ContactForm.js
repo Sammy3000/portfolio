6888f9eef750264e78e691bc6e79d8dda25b6470
Sammy3000 <towettsammy30@gmail.com>
2023-12-13 10:17:11 +0300

    Modify input name

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -23,7 +23,7 @@ const ContactForm=()=>{
         <div className='form'>
            <form ref={form} onSubmit={sendEmail}>
                 <label>Name</label>
-                <input type="text" name="user_name" />
+                <input type="text" name="from_name" />
                 <label>Email</label>
                 <input type="email" name="user_email" />
                 <label>Message</label>
